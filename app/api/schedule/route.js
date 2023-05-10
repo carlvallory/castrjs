37492e407916b7557a9585f9c46d6d55f7004419
Vallory <carlvallory@gmail.com>
2023-05-10 17:20:33 -0400

    Second commit

--- app/api/schedule/route.js
+++ app/api/schedule/route.js
@@ -3,29 +3,32 @@ import { sheetApi, SHEETID, APIKEY } from "../../utils/sheetApi";
 
 export async function GET(request) {
 
-  let url = "/spreadsheets/"+SHEETID+"/values/monday!A1:C12?key="+APIKEY;
   const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const now = new Date();
+  const today = dayNames[now.getDay()].toLowerCase();
+  const time = now.getHours() + ':' + now.getMinutes();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+
+  let url = "/spreadsheets/"+SHEETID+"/values/"+today+"!A1:C12?key="+APIKEY;
 
   const jsonData = await getJson(url);
 
   jsonData.values.forEach((row, index) => {
-    console.log(row[0]);
+    if(index != 0) {
+      if(row[0].split(":")[0] == hour) {
+        console.log(row[0]);
+      }
+    }
   });
 
-  let today = dayNames[new Date().getDay()];
   
+
   return new Response(today);
   
 }
 
 export async function getJson(url) {
   const { data } = await sheetApi.get(url);
-  return data;
-}
-
-export async function getStream(streamId) {
-  let url = "/stream/" + streamId;
-  console.log(url);
-  const { data } = await castrApi.get(url);
   return data;
 }
